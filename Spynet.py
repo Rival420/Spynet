a0a6b398b4bc145d642eaaf5d448687ff37e780a
Rival420 <51548322+Rival420@users.noreply.github.com>
2020-03-20 12:16:36 +0100

    Update Spynet.py

--- Spynet.py
+++ Spynet.py
@@ -65,6 +65,8 @@ def show_argumets():
     print(Blue + Bold + "First Port: " + NC + str(options.start_port))
     print(Blue + Bold + "Last Port: " + NC + str(options.end_port))
     print(Blue + Bold + "Delay: " + NC + str(options.default_timeout))
+    if options.verbose:
+        print(Blue + Bold + "[+] Verbosity: " + NC + "On")
 
 def print_hosts(hosts):
     for host in hosts:
